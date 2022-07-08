fb9bde32844af3bf2610e8887be3926ea762cec1
Michael Keyser <ma.keyser28@gmail.com>
2022-07-08 16:15:03 -0600

    working on lora debugging

--- ESP/test_lorawan/main/main.c
+++ ESP/test_lorawan/main/main.c
@@ -184,42 +184,31 @@ int app_main( void )
 
     Radio.SetChannel( RF_FREQUENCY );
 
-#if defined( USE_MODEM_LORA )
+    printf("radio set channel complete\n");
+
+
 
     Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
                                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
                                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
                                    true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
 
+    printf("tx config complete\n");
+    /*
     Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
                                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
                                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
                                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+    */
 
     Radio.SetMaxPayloadLength( MODEM_LORA, BUFFER_SIZE );
-
-#elif defined( USE_MODEM_FSK )
-
-    Radio.SetTxConfig( MODEM_FSK, TX_OUTPUT_POWER, FSK_FDEV, 0,
-                                  FSK_DATARATE, 0,
-                                  FSK_PREAMBLE_LENGTH, FSK_FIX_LENGTH_PAYLOAD_ON,
-                                  true, 0, 0, 0, 3000 );
-
-    Radio.SetRxConfig( MODEM_FSK, FSK_BANDWIDTH, FSK_DATARATE,
-                                  0, FSK_AFC_BANDWIDTH, FSK_PREAMBLE_LENGTH,
-                                  0, FSK_FIX_LENGTH_PAYLOAD_ON, 0, true,
-                                  0, 0,false, true );
-
-    Radio.SetMaxPayloadLength( MODEM_FSK, BUFFER_SIZE );
-
-#else
-    #error "Please define a frequency band in the compiler options."
-#endif
+    printf("Set max payload complete\n");
 
     Radio.Rx( RX_TIMEOUT_VALUE );
-
+    printf("rx complete\n");
     while( 1 )
     {
+        printf("in loop\n");
         switch( State )
         {
         case RX:
@@ -231,7 +220,7 @@ int app_main( void )
                     {
                         // Indicates on a LED that the received frame is a PONG
                         //GpioToggle( &Led4 );
-
+                        printf("Received PONG\n");
                         // Send the next PING frame
                         Buffer[0] = 'P';
                         Buffer[1] = 'I';
@@ -266,7 +255,7 @@ int app_main( void )
                     {
                         // Indicates on a LED that the received frame is a PING
                         //GpioToggle( &Led4 );
-
+                        printf("Recieved PING\n");
                         // Send the reply to the PONG string
                         Buffer[0] = 'P';
                         Buffer[1] = 'O';
@@ -292,6 +281,7 @@ int app_main( void )
         case TX:
             // Indicates on a LED that we have sent a PING [Master]
             // Indicates on a LED that we have sent a PONG [Slave]
+            printf("Sent a message\n");
             //GpioToggle( &Led3 );
             Radio.Rx( RX_TIMEOUT_VALUE );
             State = LOWPOWER;
